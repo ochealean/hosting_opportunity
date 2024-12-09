fbe55368dd9ee2817f75970ac75d9237d534f246
ochealean <ochealean@gmail.com>
2024-12-10 02:42:39 +0800

    added firstname, lastname, and ID in signup

--- firebase_control.js
+++ firebase_control.js
@@ -1,6 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
+// ID generator
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAtwT5vh0VSYQ3j-qDSjHFI3DsfdzRoCpw",
     authDomain: "opportunity-9d3bf.firebaseapp.com",
     databaseURL: "https://opportunity-9d3bf-default-rtdb.firebaseio.com",
@@ -9,32 +23,43 @@
     messagingSenderId: "57906230058",
     appId: "1:57906230058:web:4bd2c2b66a97ad34536453",
     measurementId: "G-NNMZ1KHB32"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig);
- var opportunityDB = firebase.database().ref('opportunity_db');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
- document.getElementById('signupForm').addEventListener("submit", submitForm);
+document.getElementById('signupForm').addEventListener("submit", submitForm);
 
- function submitForm(e){
+function submitForm(e) {
     e.preventDefault();
+    const userID = generateRandomString(15);
+    const opportunityDB = ref(database, 'opportunity_db/users/' + userID);
+
+    var ID = userID;
+    var firstname = getValue('firstname');
+    var lastname = getValue('lastname');
     var email = getValue('email');
     var password = getValue('password');
 
-    insertData(email, password);
- }
+    insertData(ID, firstname, lastname, email, password);
 
- function insertData(email, password)
- {
-    var newcontactform = opportunityDB.push();
+    // Log the sign-up event
+    logEvent(analytics, 'sign_up', { method: 'Email' });
+    alert('Account created');
+}
 
-    newcontactform.set({
-        user_email: email,
-        user_password: password,
-    })
- }
+function insertData(ID, firstname, lastname, email, password) {
+    set(ref(database, 'opportunity_db/users/' + ID), {
+        ID: ID,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password
+    });
+}
 
- function getValue(id)
- {
+function getValue(id) {
     return document.getElementById(id).value;
- }
+}
